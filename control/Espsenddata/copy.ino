6a17f26c8270a018c8584c80c9546e1152d3f279
Baopqspkt <44236073+Baopqspkt@users.noreply.github.com>
2019-06-12 02:20:37 +0700

    Add Eeprom test

---
+++ control/Espsenddata/copy.ino
@@ -0,0 +1,131 @@
+// Author: Pham Quoc Bao
+// Mail: baopq.spkt@gmail.com
+// Modify 11/05/2019 by qb 
+/*
+ * Transfer data from server to STM32F4 
+ * MAke file login auto
+ * 
+*/
+ 
+
+#include <ESP8266WiFi.h>
+
+const char* host = "www.baopqspkt.xyz";
+String ssid;
+String pass;
+
+char ssid1[10];
+char pass1[20];
+
+
+void setup() {
+  Serial.begin(115200);
+   /* Set ESP8266 to WiFi Station mode */
+  WiFi.mode(WIFI_STA);
+  /* start SmartConfig */
+  WiFi.beginSmartConfig();
+
+  /* Wait for SmartConfig packet from mobile */
+  Serial.println("Waiting for SmartConfig.");
+  while (!WiFi.smartConfigDone()) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("SmartConfig done.");
+
+  /* Wait for WiFi to connect to AP */
+  Serial.println("Waiting for WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("WiFi Connected.");
+  ssid = WiFi.SSID();
+  Serial.print("SSID: ");
+  Serial.println(ssid);
+  pass = WiFi.psk();
+  Serial.print("PASSWORD: ");
+  Serial.println(pass);
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+  int str_lenssid = ssid.length() + 1;   
+  int str_lenpass = pass.length() + 1;  
+  ssid.toCharArray(ssid1,str_lenssid);
+  pass.toCharArray(pass1,str_lenpass);
+  int i = 0;
+  Serial.print("ssid:");
+  for (i =0 ;i < str_lenssid;i ++)
+    Serial.print(ssid1[i]);
+  Serial.println();
+  Serial.print("pass:");
+  for (i =0 ;i < str_lenpass;i ++)
+    Serial.print(pass1[i]);
+  Serial.println();
+  
+ }
+
+void loop() 
+{
+  
+  //Serial.print("connecting to ");
+  //Serial.print(host);
+  //Serial.println();
+  //WiFiClient client;
+  
+  while(Serial.available()==0);
+  String ledsta;
+  while (Serial.available() > 0)
+    {
+        ledsta = ledsta + String(char(Serial.read()));
+        Serial.println(ledsta.length());
+        Serial.println(ledsta); 
+        //ledsta = Serial.readString();
+  if(ledsta.length() == 4)
+  {
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) 
+  {
+    Serial.println("connection failed");
+    delay(5000);
+    return;
+  }
+  String url = "/getstate.php?color=" + ledsta;
+  
+  //Serial.print("Requesting URL: ");
+  Serial.println(url);
+
+  // This will send the request to the server
+  client.print("GET " + url + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Connection: close\r\n\r\n");
+
+  String content = "";
+  while(client.connected())
+  {
+    int len = client.available();
+    if (len > 0)
+    {
+      content = content + String(char(client.read())); 
+      //content = client.readStringUntil('\r');
+    }
+  }
+  if((content.length() < 200) && (content.length()>=0))
+    {
+      content = content.substring(162,166);
+      Serial.println(content); 
+    }
+  else
+    {
+       Serial.println(content.length());
+    }
+  //Serial.println(content);
+  if(!client.connected())
+  {
+    //Serial.println("Disconnectig...");
+    //Serial.println();
+    client.stop();
+    delay(5000);
+  }
+  }
+    }
+}
--- nextion/Nextion/textcompare/textcompare.ino
+++ nextion/Nextion/textcompare/textcompare.ino
@@ -1,16 +1,19 @@
 #include <ESP8266WiFi.h>
 #include <SoftwareSerial.h>
-SoftwareSerial mySerial(4,5);
-String readString;
+SoftwareSerial mySerial(4, 5);
+
 int chophep = 0;
 const char *host = "www.baopqspkt.xyz";
 const char *ssid = "PT";
 const char *pass = "phamquocbao";
+String ledsta = "0000";
+String getdata = "1111";
 
-void setup() {
-  mySerial.begin(9600);
-  Serial.begin(9600); 
-   WiFi.mode(WIFI_STA);
+void setup()
+{
+    mySerial.begin(9600);
+    Serial.begin(9600);
+    WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, pass);
 
     Serial.println("Waiting for WiFi");
@@ -21,51 +24,101 @@ void setup() {
     }
     Serial.println("WiFi Connected.");
 }
-void endsend()
-{
-  mySerial.write(0xff);
-  mySerial.write(0xff);
-  mySerial.write(0xff);
-}
-void loop() {
 
-  while (mySerial.available()) {
-    delay(3);  
-    char c = (char)mySerial.read();
-    readString += c; 
-  }
-  readString.trim();
-  
-  if (readString.length() >0) {
-    Serial.println("Length:");
-    Serial.println(readString.length());
-    Serial.println(readString);
-  }
-  if ((readString == "11")||(readString == "01")||(readString == "12")||(readString == "02")||(readString == "13")||(readString == "03")||(readString == "14")||(readString == "04")) 
+void loop()
+{
+    if (ledsta != getdata)
     {
-      chophep = 1;
-    }
-  if (chophep == 1)
-    {
-      WiFiClient client;
-    const int httpPort = 80;
-    if (!client.connect(host, httpPort))
+        ledsta = getdata;
+        if (ledsta.length() > 0)
         {
-            Serial.println("connection failed");
-            delay(5000);
-            return;
+            WiFiClient client;
+            const int httpPort = 80;
+            if (!client.connect(host, httpPort))
+            {
+                Serial.println("connection failed");
+                delay(5000);
+                return;
+            }
+            String url = "/getstate.php?color=" + ledsta;
+            Serial.println(url);
+            client.print("GET " + url + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Connection: close\r\n\r\n");
+            String content = "";
+            while (client.connected())
+            {
+                int len = client.available();
+                if (len > 0)
+                {
+                    content = content + String(char(client.read()));
+                }
+            }
+            if ((content.length() < 200) && (content.length() >= 0))
+            {
+                content = content.substring(162, 166);
+                //Serial.println(content);
+                //Serial.println(content.length());
+            }
+            else
+            {
+                Serial.println(content.length());
+            }
+            client.stop();
+            if (content.substring(0, 1) == "1")
+            {
+                String command = "0x11";
+                mySerial.print(command);
+            }
+            else if (content.substring(0, 1) == "0")
+            {
+                String command = "0x01";
+                mySerial.print(command);
+            }
+            if (content.substring(1, 2) == "1")
+            {
+                String command = "0x12";
+                mySerial.print(command);
+            }
+            else if (content.substring(1, 2) == "0")
+            {
+                String command = "0x02";
+                mySerial.print(command);
+            }
+            if (content.substring(2, 3) == "1")
+            {
+                String command = "0x13";
+                mySerial.print(command);
+            }
+            else if (content.substring(2, 3) == "0")
+            {
+                String command = "0x03";
+                mySerial.print(command);
+            }
+            if (content.substring(3, 4) == "1")
+            {
+                String command = "0x14";
+                mySerial.print(command);
+            }
+            else if (content.substring(3, 4) == "0")
+            {
+                String command = "0x04";
+                mySerial.print(command);
+            }
+            chophep = 0;
+            getdata = "";
         }
-    String url = "/getdatanextion.php?color=";
-    url = url + readString;
-    Serial.println(url);
-    client.print("GET " + url + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Connection: close\r\n\r\n");
-    String content = "";
-    client.stop();
-    chophep = 0;
-    readString = "";
     }
-   else
+    while (mySerial.available())
     {
-      readString = "";
+        delay(3);
+        char c = (char)mySerial.read();
+        getdata += c;
+    }
+    getdata.trim();
+
+    if (getdata.length() > 0)
+    {
+        Serial.println("Length:");
+        Serial.println(getdata.length());
+        Serial.println(getdata);
     }
 }
